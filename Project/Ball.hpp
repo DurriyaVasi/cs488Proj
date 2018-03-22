598d0e407bd256ddd2a299c6dbbefadb2078ec43
Durriya Vasi <dvasi@uwaterloo.ca>
2018-03-22 10:42:26 -0400

    added some changes to ball for collsions

--- Project/Ball.hpp
+++ Project/Ball.hpp
@@ -2,24 +2,50 @@
 #include "CollisionType.hpp"
 #include <cmath>
 
+bool lessThanEqualTo(float a, float b) {
+	return !(a > b);
+}
+
+bool greaterThanEqualTo(float a, float b) {
+	return !(a < b);
+}
+
 class Ball {
-	glm::vec2 pos;
-	float radius;
-	glm::vec2 direction;
-	float speed;
+	glm::vec2 m_pos;
+	float m_radius;
+	glm::vec2 m_direction;
+	float m_speed;
 	
 	public:
-	Ball(glm::vec2 pos, glm::vec2 direction, float speed) 
-		: pos(pos),
-		  direction(direction),
-		  speed(speed) {}
+	Ball(glm::vec2 pos, float radius, glm::vec2 direction, float speed) 
+		: m_pos(pos),
+		  m_radius(radius),
+		  m_direction(direction),
+		  m_speed(speed) {}
 
 	std::pair<CollsionType, mat4> move(Paddle p, Board b) {
-		glm::vec3 newPos = pos + (speed * direction);
+		glm::vec3 newPos = m_pos + (speed * direction);
 		
+		float newCircleBottom = newPos.y - radius;
+		float newCircleLeft = newPos.x - radius;
+		float newCircleRight = newPos.x + radius;
+		float newCircleTop = newPos.y + radius;
+
 		// check if hit bottom or paddles
-		if (((pos.y - radius) < (b.pos + b.height)) && (pos.x + radius ) {
-			  
+		float paddleTop = b.pos + b.height;
+		if (lessThanEqualTo((newPos.y - radius), paddleTop) {
+		
+			// find position where it hits y of board top
+			glm::vec3 tToPaddleTop = (paddleTop - (m_pos.y))/(direction.y)
+	
+				
+
+			if ( (lessThanEqualTo((newPos.x + radius), (b.pos + b.width)) &&
+				greaterThanEqualTo((newPos.x + radius), (b.pos - b.width))) ||
+			     (lessThanEqualTo((newPos.x - radius), (b.pos + b.width)) &&
+				greaterThanEqualTo((newPos.x - radius), (b.pos - b.width)))  ) {
+				
+				
 		
 
 		// check if hit boundaries
@@ -27,16 +53,16 @@ class Ball {
 		float tY = 0;
 		bool hitX = false;
 		bool hitY = false;
-		if (newPos.x >= b.highXBoundary) {
-			tX = newPos.x - b.highXBoundary;
+		if (newCircleRight >= b.highXBoundary) {
+			tX = (b.highXBoundary - m_pos.x)/(direction.x);
 			hitX = true;
 		}
-		else if (newPos.x <= b.lowXBoundary) {
-			tX = lowXBoundary - newPos.x;
+		else if (newCircleLeft <= b.lowXBoundary) {
+			tX = (b.lowXBoundary - m_pos.x)/(direction.x);
 			hitX = true;
 		}
-		if (newPos.y >= b.highYBoundary) {
-			tY = newPos.y - b.highYBoundary;
+		if (newCircleTop >= b.highYBoundary) {
+			tY = (b.highYBoundary - m_pos.y)/(direction.y);
 			hitY = true;
 		}
 		
